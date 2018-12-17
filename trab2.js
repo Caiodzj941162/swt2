2b0476a445d6a1cdc04a7ef659e9074aab12617d
caio <aaaaa@bbbb.com>
2018-12-17 19:12:31 -0300

    atualiza info pra montar url

--- trab2.js
+++ trab2.js
@@ -1,9 +1,13 @@
 function onLoadDoc() {
     var xmlhttp = new XMLHttpRequest();
     var url_parte1 = "https://newsapi.org/v2/top-headlines?country=";
-    var url_parte2 = "&apiKey=";
+    var url_parte2 = "&q=";
+    var url_parte3 = "&apiKey=";
     var key = "api_key_aqui";
-    //Somar url_parte1 + pais_input + url_parte2 + key pra obter url final
+    /*
+    Somar url_parte1 + pais_input + url_parte2 + keyword_input + 
+    url_parte3 + key pra obter url final
+    */
     var pais_input = document.getElementById("pais_input").value;
     var keyword_input = document.getElementById("keyword_input").value;
 
