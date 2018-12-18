d2166754174c957847662eedd7e02ecf3c392ab8
caio <aaaaa@bbbb.com>
2018-12-18 19:32:54 -0300

    Busca de notícias funcionando, adiciona na página

--- trab2.js
+++ trab2.js
@@ -1,24 +1,23 @@
 function pegarNews() {
-    //var xmlhttp = new XMLHttpRequest();
-    console.log("Chamou a função");
-    var url_parte1 = "https://newsapi.org/v2/top-headlines?country=";
-    var url_parte2 = "&q=";
+    url_parte1 = "https://newsapi.org/v2/everything?q=";
     var url_parte3 = "&apiKey=";
     var key = "chave_aqui";
     var pais_input = document.getElementById("pais_input").value;
     console.log("País: " + pais_input);
     var keyword_input = document.getElementById("keyword_input").value;
     console.log("Keyword: " + keyword_input);
-    var url = url_parte1 + pais_input + url_parte2 + keyword_input + 
-    url_parte3 + key;
+    var url = url_parte1 + keyword_input + url_parte3 + key;
     console.log("URL: " + url);
-    /*
-    Somar url_parte1 + pais_input + url_parte2 + keyword_input + 
-    url_parte3 + key pra obter url final
-    */
-    var req = new Request(url);
-    fetch(req).then(function(response) {
-            console.log(response.json());
-            //parsear JSON recebido aqui
-        })
+    var espaco_news = document.getElementById("div_da_news");
+    espaco_news.innerHTML = "";
+    fetch(url)
+    .then(response => response.json())
+    .then(function(data) {
+        for (var i = 0; i < data.articles.length; i++) {
+            espaco_news.innerHTML += "<h3>" + data.articles[i].title + "</h3>"
+            + "<img src = '" + data.articles[i].urlToImage + "'/>"
+            + "<h4>" + data.articles[i].description + "</h4><a href='"
+            + data.articles[i].url + "'>Link</a><br><br>";
+        }
+ });
 }
