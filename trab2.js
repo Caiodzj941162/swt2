96778b81e17bf29f65223d677e4acb0e7ed49f43
Lucas Souza <alfsouza708@gmail.com>
2018-12-18 23:23:20 -0300

    api twitter retornando falha, mas implementada

--- trab2.js
+++ trab2.js
@@ -1,22 +1,38 @@
 function pegarNews() {
-    //var xmlhttp = new XMLHttpRequest();
-    console.log("Chamou a função");
-    var url_parte1 = "https://newsapi.org/v2/top-headlines?country=";
-    var url_parte2 = "&q=";
+    //console.log("Chamou a função");
+    var url_parte1 = "https://newsapi.org/v2/everything?";
+    var url_parte2 = "q=";
     var url_parte3 = "&apiKey=";
-    var key = "chave_aqui";
-    var pais_input = document.getElementById("pais_input").value;
-    console.log("País: " + pais_input);
+    var key = "21abd04126a0427baf5922b6643ed7d1";
+    //var pais_input = document.getElementById("pais_input").value;
+    //console.log("País: " + pais_input);
     var keyword_input = document.getElementById("keyword_input").value;
-    console.log("Keyword: " + keyword_input);
-    var url = url_parte1 + pais_input + url_parte2 + keyword_input + 
+    //console.log("Keyword: " + keyword_input);
+    var url = url_parte1 + url_parte2 + keyword_input + 
     url_parte3 + key;
-    console.log("URL: " + url);
+    //console.log("URL: " + url);
     /*
     Somar url_parte1 + pais_input + url_parte2 + keyword_input + 
     url_parte3 + key pra obter url final
     */
     var req = new Request(url);
+    fetch(req).then(function(response) {
+            console.log(response.json());
+            //parsear JSON recebido aqui
+        })
+}
+
+function pegarTweets() {
+    console.log("CHAMOU A FUNÇÃO");
+    var url_part1 = "https://api.twitter.com/1.1/search/tweets.json?";
+    var url_part2 = "q=";
+    var keyword_input_tweet = document.getElementById("keyword_input").value;
+
+    var url_tweet = url_part1 + url_part2 + keyword_input_tweet;
+    console.log("Url: " + url_tweet);
+    console.log("Keyword: " + keyword_input_tweet);
+
+    var req = new Request(url_tweet);
     fetch(req).then(function(response) {
             console.log(response.json());
             //parsear JSON recebido aqui
